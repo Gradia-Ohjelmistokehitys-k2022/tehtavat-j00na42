64d290d73783a17b5f6da648a8cff6e92124cdf1
Joona  Virtanen <virtanenjoona478@gmail.com>
2024-11-28 08:57:57 +0200

    T3.1 aloitettu.

---
+++ T3/T3_1_Bitcoin_market_analyzer/BitcoinMarketAnalyzer/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using BitcoinMarketAnalyzer.Model;
+
+
+
+namespace BitcoinMarketAnalyzer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("Testi");
+
+        }
+    }
+}
---
+++ T3/T3_1_Bitcoin_market_analyzer/BitcoinMarketAnalyzer/Model/Bitcoin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace BitcoinMarketAnalyzer.Model
+{
+    public class Bitcoin // DB Model
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public decimal Price { get; set; }
+        public decimal Market_cap { get; set; }
+        public decimal Total_volume { get; set; }
+    }
+
+    public class BitcoinJsonResponse // JSON response model
+    { 
+        public string[,] Prices { get; set; }
+        public string[,] Market_caps { get; set; }
+        public string[,] Total_volumes { get; set; }
+    }
+}
